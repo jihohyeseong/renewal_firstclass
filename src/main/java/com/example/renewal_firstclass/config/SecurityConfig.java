644ec925b58dbf6842aab9ccb5f21a44f9da68c2
jihohyeseong <hsl4720@naver.com>
2025-10-17 15:45:49 +0900

    기업조건추가

--- src/main/java/com/example/renewal_firstclass/config/SecurityConfig.java
+++ src/main/java/com/example/renewal_firstclass/config/SecurityConfig.java
@@ -36,25 +36,32 @@ public class SecurityConfig {
 	            ).hasRole("ADMIN")
 	            .anyRequest().authenticated();
         
-        http
-        	.formLogin()
-        		.loginPage("/login")
-                .loginProcessingUrl("/loginProc")
-                .successHandler((request, response, authentication) -> {
-                    // 사용자의 권한(role)을 확인
-                    boolean isAdmin = authentication.getAuthorities().stream()
-                            .map(GrantedAuthority::getAuthority)
-                            .anyMatch(auth -> auth.equals("ROLE_ADMIN"));
+		http
+	    .formLogin()
+	        .loginPage("/login")
+	        .loginProcessingUrl("/loginProc")
+	        .successHandler((request, response, authentication) -> {
+	            // 사용자의 권한(role)을 확인
+	            boolean isAdmin = authentication.getAuthorities().stream()
+	                    .map(GrantedAuthority::getAuthority)
+	                    .anyMatch(auth -> auth.equals("ROLE_ADMIN"));
 
-                    if (isAdmin) {
-                        // ADMIN이면 /adminlist로 리디렉션
-                        response.sendRedirect("/renewal_firstclass/admin/applications");
-                    } else {
-                        // 일반 사용자이면 /main으로 리디렉션
-                        response.sendRedirect("/renewal_firstclass/main");
-                    }
-                })
-                .permitAll();
+	            boolean isCorp = authentication.getAuthorities().stream()
+	                    .map(GrantedAuthority::getAuthority)
+	                    .anyMatch(auth -> auth.equals("ROLE_CORP"));
+
+	            if (isAdmin) {
+	                // ADMIN이면 /adminlist로 리디렉션
+	                response.sendRedirect("/renewal_firstclass/admin/applications");
+	            } else if (isCorp) {
+	                // 기업 사용자면 /comp/main으로 리디렉션
+	                response.sendRedirect("/renewal_firstclass/comp/main");
+	            } else {
+	                // 일반 사용자이면 /main으로 리디렉션
+	                response.sendRedirect("/renewal_firstclass/main");
+	            }
+	        })
+	        .permitAll();
         
         http.csrf().disable();
 
